4e52e5b9174f7e5ef8a49c68a12ef20d27c05087
HuyTran0996 <tranviquanghuy@gmail.com>
2024-02-26 21:32:11 +0700

    2024-02-26

--- front_end/src/app/apiService.js
+++ front_end/src/app/apiService.js
@@ -1,8 +1,7 @@
 import axios from "axios";
-import { BASE_URL, BASE_URL123 } from "./config";
 
 const apiService = axios.create({
-  baseURL: BASE_URL,
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 apiService.interceptors.request.use(
@@ -32,34 +31,4 @@ apiService.interceptors.response.use(
 );
 ///////////////////////////////////////////////
 
-const apiServiceLogin = axios.create({
-  baseURL123: BASE_URL123,
-});
-
-apiServiceLogin.interceptors.request.use(
-  (request) => {
-    console.log("Start Login request", request);
-    return request;
-  },
-  function (error) {
-    console.log("REQUEST Login ERROR", error);
-  }
-);
-
-apiServiceLogin.interceptors.response.use(
-  (response) => {
-    console.log("Response Login", response);
-
-    return response;
-  },
-  function (error) {
-    console.log("RESPONSE Login ERROR", error);
-    console.log("RESPONSE Login ERROR", error.response.data.error);
-
-    return Promise.reject(error);
-
-    // return errorName;
-  }
-);
-
-export { apiService, apiServiceLogin };
+export { apiService };
