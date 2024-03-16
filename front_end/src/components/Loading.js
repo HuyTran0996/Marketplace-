b4eff2fe9ba8eceb89cb9e9d4b20d494e31aa73c
HuyTran0996 <tranviquanghuy@gmail.com>
2024-03-16 15:40:20 +0700

    2024-03-16

--- front_end/src/components/Loading.js
+++ front_end/src/components/Loading.js
@@ -1,22 +1,53 @@
 import React from "react";
 import Skeleton from "@mui/material/Skeleton";
+import Box from "@mui/material/Box";
 
 const Loading = () => {
   const skeletons = [];
 
   for (let i = 0; i < 4; i++) {
     skeletons.push(
-      <Skeleton
+      <Box
         key={i}
-        className="widgets"
-        variant="rectangular"
-        width="80%"
-        height={40}
-      />
+        display="flex"
+        flexDirection="column"
+        alignItems="flex-start"
+      >
+        <Skeleton
+          className="widgets"
+          animation="wave"
+          variant="rectangular"
+          width="60%"
+          height={70}
+        />
+
+        <Skeleton
+          className="widgets"
+          animation="wave"
+          variant="rectangular"
+          width="40%"
+          height={1}
+        />
+        <Skeleton
+          className="widgets"
+          animation="wave"
+          variant="rectangular"
+          width="10%"
+          height={1}
+        />
+      </Box>
     );
   }
 
-  return <div>{skeletons}</div>;
+  return (
+    <Box
+      display="grid"
+      gridTemplateColumns="repeat(2, 1fr)"
+      gap={2} // Adjust the gap between grid items as needed
+    >
+      {skeletons}
+    </Box>
+  );
 };
 
 export default Loading;
